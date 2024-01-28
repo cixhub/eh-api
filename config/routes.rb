efb138a9f302563fceabee5e687bd691ce3b5e60
RileyManda <rileymanda69@gmail.com>
2024-01-28 23:25:47 +0200

    Bug fix | updated health check to route

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get "/", to: "rails/health#show", as: :rails_health_check
   get '*path', to: 'pages#home', constraints: lambda { |req| req.format.html? }
 
 end
