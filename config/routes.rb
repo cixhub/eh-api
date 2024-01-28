a962e297c58f599cffab7646cc569ad3b30ba45c
RileyManda <rileymanda69@gmail.com>
2024-01-28 22:47:00 +0200

    Bug fix:removed pages/home

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,6 @@ Rails.application.routes.draw do
     root 'dashboard#index', as: :authenticated_root
   end
 
-  unauthenticated do
-    root 'pages#home'
-  end
-
   namespace :api do
     namespace :v1 do
       resources :participants, only: [:index, :show, :create, :update, :destroy]
@@ -30,7 +26,6 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  get '*path', to: 'pages#home', constraints: lambda { |req| req.format.html? }
 
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
