5a6bb65857759d668600ee765c3222a22db8b0d1
RileyManda <rileymanda69@gmail.com>
2024-01-31 17:06:54 +0200

    Removed pages route

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,5 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "/", to: "rails/health#show", as: :rails_health_check
-  get '*path', to: 'pages#home', constraints: lambda { |req| req.format.html? }
 
 end
